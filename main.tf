3188c338818924d3344b36ff4155f9fe53707ee1
Jeroen Zaal <jeroen@zaal.io>
2022-01-26 14:22:34 +0100

    Add resource rg

--- main.tf
+++ main.tf
@@ -13,3 +13,21 @@ provider "azurerm" {
     features {}
 }
 
+resource "azurerm_resource_group" "rg" {
+  name = "rg-weu-test-001"
+  location = "westeurope"
+  tags = {
+    "costcenter" = "personal"
+    "environment" = "test"
+  }
+}
+
+# resource "azurerm_consumption_budget_resource_group" "rg_budget" {
+#   name = "${azurerm_resource_group.DefaultResourceGroup-WEU}_budget"
+#   amount = 50
+#   time_grain = "Monthly"
+#   time_period {
+#     start_date = formatdate("2022-01-01'T'0:0:0Z",timestamp())
+#   }
+#   resource_group_id = azurerm_resource_group./subscriptions/b8595241-20b8-4f79-acff-f3be0a5ee472/resourceGroups/DefaultResourceGroup-WEU
+# }
